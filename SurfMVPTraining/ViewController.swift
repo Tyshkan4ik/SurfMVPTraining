92d48e22877ab339459e30d7d846aea316387551
Tyshkan4ik <001vat@gmail.com>
2023-09-14 09:56:19 +0300

    first commit

---
+++ SurfMVPTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfMVPTraining
+//
+//  Created by Виталий Троицкий on 14.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
